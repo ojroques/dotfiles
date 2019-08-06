b461f0608c3040f1de36361351f8f814fba78a6c
Olivier Roques <oroques@fastmail.com>
2019-08-06 12:55:32 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,37 +3,30 @@
 CONFIG_DIR="$HOME/.config"
 
 echo "DOTFILES CONFIGURATION"
-
 echo "------------------------------------------------------------"
 if [[ ! -d "$CONFIG_DIR" ]]; then
     echo "Creating $CONFIG_DIR..."
     mkdir "$CONFIG_DIR"
 fi
 
-echo -e "[BASH]"
+echo "[BASH]"
 cp -v bash/.profile $HOME
 cp -v bash/.bashrc $HOME
 cp -v bash/.aliases $HOME
 cp -v bash/.functions $HOME
-
 echo "[GIT]"
 cp -v git/.gitconfig $HOME
-
-echo "[TMUX]"
-cp -v tmux/.tmux.conf $HOME
-
-echo "[KITTY]"
-cp -v -r kitty/ $CONFIG_DIR
-
-echo "[ROFI]"
-cp -v -r rofi/ $CONFIG_DIR
-
 echo "[VIM]"
 cp -v vim/.vimrc $HOME
-
+echo "[TMUX]"
+cp -v tmux/.tmux.conf $HOME
 echo "[NEOVIM]"
-cp -v -r nvim/ $CONFIG_DIR
+cp -vr nvim/ $CONFIG_DIR
 rm -f $CONFIG_DIR/nvim/lsp.sh
+echo "[KITTY]"
+cp -vr kitty/ $CONFIG_DIR
+echo "[ROFI]"
+cp -vr rofi/ $CONFIG_DIR
 
 if [[ -d "$CONFIG_DIR/xfce4/xfconf/xfce-perchannel-xml" ]]; then
     echo "[THUNAR]"
