d18999ad09fa1357878c7a748c319b6112ed6b34
Olivier Roques <oroques@fastmail.com>
2020-02-04 11:25:05 +0100

    Remove shellcheck warnings from scripts

--- latex/latex.sh
+++ latex/latex.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
 echo "[Update]"
 sudo apt update
-echo -e "\n[texlive-full]"
+echo -e "\\n[texlive-full]"
 sudo apt install -y texlive-full
-echo -e "\n[latexmk]"
+echo -e "\\n[latexmk]"
 sudo apt install -y latexmk
-echo -e "\n[xdotool]"
+echo -e "\\n[xdotool]"
 sudo apt install -y xdotool
-echo -e "\n[neovim-remote]"
+echo -e "\\n[neovim-remote]"
 pip3 install --user neovim-remote
