5008da287eb2b3133ca0dfee50349851fe3eeb76
Olivier Roques <olivier@oroques.dev>
2021-05-16 11:18:41 +0200

    Add vim-unimpaired plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,7 +30,6 @@ paq {'kosayoda/nvim-lightbulb'}
 paq {'lervag/vimtex'}
 paq {'lukas-reineke/indent-blankline.nvim', branch = 'lua'}
 paq {'machakann/vim-sandwich'}
-paq {'michaeljsmith/vim-indent-object'}
 paq {'neovim/nvim-lspconfig'}
 paq {'nvim-treesitter/nvim-treesitter'}
 paq {'ojroques/nvim-bufbar'}
@@ -44,6 +43,7 @@ paq {'shougo/deoplete-lsp'}
 paq {'shougo/deoplete.nvim'}
 paq {'tpope/vim-commentary'}
 paq {'tpope/vim-fugitive'}
+paq {'tpope/vim-unimpaired'}
 
 -------------------- PLUGIN SETUP --------------------------
 -- bufbar
@@ -133,26 +133,18 @@ map('n', '<C-l>', '<cmd>nohlsearch<CR>')
 map('n', '<C-w>T', '<cmd>tabclose<CR>')
 map('n', '<C-w>t', '<cmd>tabnew<CR>')
 map('n', '<F3>', '<cmd>lua toggle_wrap()<CR>')
-map('n', '<F4>', '<cmd>set spell!<CR>')
+map('n', '<F4>', '<cmd>set scrollbind!<CR>')
 map('n', '<F5>', '<cmd>checktime<CR>')
-map('n', '<F6>', '<cmd>set scrollbind!<CR>')
 map('n', '<S-Down>', '<C-w>2<')
 map('n', '<S-Left>', '<C-w>2-')
 map('n', '<S-Right>', '<C-w>2+')
 map('n', '<S-Up>', '<C-w>2>')
-map('n', '<leader><Down>', '<cmd>cclose<CR>')
-map('n', '<leader><Left>', '<cmd>cprev<CR>')
-map('n', '<leader><Right>', '<cmd>cnext<CR>')
-map('n', '<leader><Up>', '<cmd>copen<CR>')
-map('n', '<leader>o', 'm`o<Esc>0D``')
 map('n', '<leader>s', ':%s//gcI<Left><Left><Left><Left>')
 map('n', '<leader>t', '<cmd>terminal<CR>')
 map('n', '<leader>u', '<cmd>update<CR>')
 map('n', '<leader>x', '<cmd>conf qa<CR>')
 map('n', 'Q', '<cmd>lua warn_caps()<CR>')
-map('n', 'S', '<cmd>bn<CR>')
 map('n', 'U', '<cmd>lua warn_caps()<CR>')
-map('n', 'X', '<cmd>bp<CR>')
 map('t', '<ESC>', '&filetype == "fzf" ? "\\<ESC>" : "\\<C-\\>\\<C-n>"' , {expr = true})
 map('t', 'jj', '<ESC>', {noremap = false})
 map('v', '<leader>s', ':s//gcI<Left><Left><Left><Left>')
