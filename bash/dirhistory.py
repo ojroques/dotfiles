9d4a572324737cd1ad2a25af039512772acba792
Olivier Roques <oroques@fastmail.com>
2021-06-08 16:49:24 +0000

    Update neovim config

--- bash/dirhistory.py
+++ bash/dirhistory.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
+from os import environ
 from pathlib import Path
 
-DIR_HISTORY = Path(Path.home(), ".dir_history")
-MAX_HISTORY = 100
+DIR_HISTORY_FILE = Path(environ.get("DIR_HISTORY_FILE",
+                                    "~/.dir_history")).expanduser()
+DIR_HISTORY_MAX = environ.get("DIR_HISTORY_MAX", 100)
 
 
 def log_dir(dir):
     if not Path.is_dir(Path(dir).expanduser()):
         return
 
-    Path.touch(DIR_HISTORY, exist_ok=True)
+    Path.touch(DIR_HISTORY_FILE, exist_ok=True)
 
-    with open(DIR_HISTORY, "r+") as f:
+    with open(DIR_HISTORY_FILE, "r+") as f:
         history = f.readlines()
 
+        # If directory has not changed, return early
         if history and history[-1].strip() == dir:
             return
 
         f.seek(0)
-        start = 1 - MAX_HISTORY if len(history) > MAX_HISTORY - 1 else 0
+        start = 1 - DIR_HISTORY_MAX if len(history) >= DIR_HISTORY_MAX else 0
 
         for d in history[start:]:
             if d.strip() != dir:
@@ -30,10 +33,10 @@ def log_dir(dir):
 
 
 def show_history():
-    if not Path.is_file(DIR_HISTORY):
+    if not Path.is_file(DIR_HISTORY_FILE):
         return
 
-    with open(DIR_HISTORY, "r") as f:
+    with open(DIR_HISTORY_FILE, "r") as f:
         for d in reversed(f.readlines()):
             print(d, end="")
 
@@ -52,7 +55,8 @@ if __name__ == "__main__":
         if sys.argv[1] == "log":
             if len(sys.argv) != 3:
                 usage()
-            log_dir(sys.argv[2])
+            else:
+                log_dir(sys.argv[2])
             return
 
         if sys.argv[1] == "show":
