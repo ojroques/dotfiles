0ef2f594184a0d3a2db6997b89d4bf8ce563f199
Olivier Roques <olivier@oroques.dev>
2021-06-27 10:41:20 +0200

    Update vim and neovim configs

--- config.sh
+++ config.sh
@@ -16,7 +16,6 @@ mkdir -vp "$HOME"/Downloads
 echo "[BASH]"
 cp -vr bash/.shell "$HOME"
 cp -v bash/.bashrc "$HOME"
-cp -v bash/dirhistory.py "$HOME"/.local/bin/dirhistory
 
 echo "[GIT]"
 cp -v git/.gitconfig "$HOME"
