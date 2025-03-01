e9eecfa8d89a4be9d921aba0da3bafba92a7fa0e
Olivier Roques <oroques@fastmail.com>
2025-03-02 00:21:28 +0100

    feat(neovim): update config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,60 +7,60 @@ if not vim.uv.fs_stat(mini) then
 end
 
 -------------------- PLUGINS ---------------------------------------------------
-require('mini.deps').setup {}
-MiniDeps.add('navarasu/onedark.nvim')
-MiniDeps.add('neovim/nvim-lspconfig')
-MiniDeps.add('nvim-treesitter/nvim-treesitter')
-MiniDeps.add('nvim-treesitter/nvim-treesitter-context')
+require('mini.deps').setup()
+MiniDeps.add({source = 'navarasu/onedark.nvim', checkout = 'master'})
+MiniDeps.add({source = 'neovim/nvim-lspconfig', checkout = 'master'})
+MiniDeps.add({source = 'nvim-treesitter/nvim-treesitter', checkout = 'master'})
+MiniDeps.add({source = 'nvim-treesitter/nvim-treesitter-context', checkout = 'master'})
+MiniDeps.add({source = 'ojroques/nvim-bookmarks', checkout = 'main'})
+MiniDeps.add({source = 'ojroques/nvim-bufbar', checkout = 'personal'})
 
 -------------------- PLUGIN SETUP ----------------------------------------------
 -- mini.extra
-require('mini.extra').setup {}
+require('mini.extra').setup()
 -- mini.ai
-require('mini.ai').setup {custom_textobjects = {e = MiniExtra.gen_ai_spec.buffer()}}
+require('mini.ai').setup({custom_textobjects = {e = MiniExtra.gen_ai_spec.buffer()}})
 -- mini.basics
-require('mini.basics').setup {
+require('mini.basics').setup({
   options = {basic = false},
   mappings = {option_toggle_prefix = 'yo'},
-}
+})
 -- mini.bracketed
-require('mini.bracketed').setup {}
+require('mini.bracketed').setup()
 -- mini.bufremove
-require('mini.bufremove').setup {}
+require('mini.bufremove').setup()
 vim.keymap.set('n', 'gd', MiniBufremove.delete)
 -- mini.completion
-require('mini.completion').setup {lsp_completion = {source_func = 'omnifunc', auto_setup = false}}
+require('mini.completion').setup({lsp_completion = {source_func = 'omnifunc', auto_setup = false}})
 vim.keymap.set('i', '<Tab>', function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>' end, {expr = true})
 vim.keymap.set('i', '<S-Tab>', function() return vim.fn.pumvisible() == 1 and '<C-p>' or '<S-Tab>' end, {expr = true})
 -- mini.diff
-require('mini.diff').setup {view = {signs = {add = '+', change = '~', delete = '-'}}}
+require('mini.diff').setup()
 vim.keymap.set('n', 'ghp', MiniDiff.toggle_overlay)
 vim.keymap.set('n', 'ghR', 'gHae', {remap = true})
 vim.keymap.set('n', 'ghS', 'ghae', {remap = true})
 vim.keymap.set('n', 'ghr', 'gHgh', {remap = true})
 vim.keymap.set('n', 'ghs', 'ghgh', {remap = true})
 -- mini.files
-require('mini.files').setup {}
+require('mini.files').setup()
 vim.keymap.set('n', '-', function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end)
--- mini.git
-require('mini.git').setup {}
 -- mini.icons
-require('mini.icons').setup {}
+require('mini.icons').setup()
 MiniDeps.later(MiniIcons.tweak_lsp_kind)
 -- mini.indentscope
-require('mini.indentscope').setup {draw = {animation = require('mini.indentscope').gen_animation.none()}}
+require('mini.indentscope').setup({draw = {animation = require('mini.indentscope').gen_animation.none()}})
 -- mini.misc
 require('mini.misc').setup_auto_root({'.git'})
 -- mini.notify
-require('mini.notify').setup {}
+require('mini.notify').setup()
 vim.notify = MiniNotify.make_notify()
 -- mini.operators
-require('mini.operators').setup {
-  evaluate = {prefix = ''}, multiply = {prefix = ''},
+require('mini.operators').setup({
   exchange = {prefix = 'cx'}, replace = {prefix = 'cr'},
-}
+  evaluate = {prefix = ''}, multiply = {prefix = ''},
+})
 -- mini.pick
-require('mini.pick').setup {mappings = {refine = '<C-q>', refine_marked = '<M-q>'}}
+require('mini.pick').setup({mappings = {refine = '<C-q>', refine_marked = '<M-q>'}})
 vim.keymap.set('n', 'sb', MiniPick.builtin.buffers)
 vim.keymap.set('n', 'sd', MiniExtra.pickers.diagnostic)
 vim.keymap.set('n', 'sf', MiniPick.builtin.files)
@@ -68,45 +68,53 @@ vim.keymap.set('n', 'sg', MiniPick.builtin.grep)
 vim.keymap.set('n', 'sl', MiniPick.builtin.grep_live)
 vim.keymap.set('n', 'sr', MiniPick.builtin.resume)
 -- mini.statusline
-require('mini.statusline').setup {}
+require('mini.statusline').setup()
 -- mini.surround
-require('mini.surround').setup {mappings = {
+require('mini.surround').setup({mappings = {
   add = 'ys', delete = 'ds', replace = 'cs',
-  find = '', find_left = '', highlight = '', update_n_lines = '', suffix_last = '', suffix_next = '',
-}}
+  find = '', find_left = '', highlight = '', suffix_last = '', suffix_next = '', update_n_lines = '',
+}})
 vim.keymap.del('x', 'ys')
 -- mini.trailspace
-require('mini.trailspace').setup {}
+require('mini.trailspace').setup()
+-- nvim-bookmarks
+local bookmarks = require('bookmarks')
+bookmarks.setup()
+vim.keymap.set('n', 'mm', bookmarks.add)
+vim.keymap.set('n', 'm1', function() bookmarks.open(1) end)
+vim.keymap.set('n', 'm2', function() bookmarks.open(2) end)
+vim.keymap.set('n', 'm3', function() bookmarks.open(3) end)
+vim.keymap.set('n', 'm4', function() bookmarks.open(4) end)
+vim.keymap.set('n', 'M', bookmarks.toggle_menu)
+-- nvim-bufbar
+require('bufbar').setup()
 -- nvim-treesitter
-require('nvim-treesitter.configs').setup {
+require('nvim-treesitter.configs').setup({
   ensure_installed = 'all',
   highlight = {enable = true},
-}
+})
 -- nvim-treesitter-context
-require('treesitter-context').setup {mode = 'topline', separator = '━'}
+require('treesitter-context').setup({mode = 'topline', separator = '━'})
 -- onedark.nvim
 require('onedark').load()
--- tabline
-require('tabline').setup {}
 
 -------------------- LSP -------------------------------------------------------
+local on_attach = function(_, buf)
+  vim.bo[buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+  vim.keymap.set('n', 'gqae', vim.lsp.buf.format, {buffer = buf})
+  vim.keymap.set('n', 'grd', function() MiniExtra.pickers.lsp({scope = 'definition'}) end, {buffer = buf})
+  vim.keymap.set('n', 'gri', function() MiniExtra.pickers.lsp({scope = 'implementation'}) end, {buffer = buf})
+  vim.keymap.set('n', 'grr', function() MiniExtra.pickers.lsp({scope = 'references'}) end, {buffer = buf})
+  vim.keymap.set('n', 'grt', function() MiniExtra.pickers.lsp({scope = 'type_definition'}) end, {buffer = buf})
+end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false
-for _, ls in ipairs({'bashls', 'gopls', 'pylsp'}) do
-  require('lspconfig')[ls].setup {
-    capabilities = capabilities,
-    on_attach = function(_, buf)
-      vim.bo[buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
-      vim.keymap.set('n', 'gqae', vim.lsp.buf.format, {buffer = buf})
-      vim.keymap.set('n', 'grd', function() MiniExtra.pickers.lsp({scope = 'definition'}) end, {buffer = buf})
-      vim.keymap.set('n', 'gri', function() MiniExtra.pickers.lsp({scope = 'implementation'}) end, {buffer = buf})
-      vim.keymap.set('n', 'grr', function() MiniExtra.pickers.lsp({scope = 'references'}) end, {buffer = buf})
-      vim.keymap.set('n', 'grt', function() MiniExtra.pickers.lsp({scope = 'type_definition'}) end, {buffer = buf})
-    end,
-  }
+for _, ls in ipairs({'bashls', 'gopls', 'pylsp', 'terraformls'}) do
+  require('lspconfig')[ls].setup({capabilities = capabilities, on_attach = on_attach})
 end
 
 -------------------- OPTIONS ---------------------------------------------------
+vim.diagnostic.config({severity_sort = true, virtual_text = true})
 vim.opt.colorcolumn = '+1'                    -- Line length marker
 vim.opt.completeopt = {'menuone', 'noselect'} -- Completion options
 vim.opt.cursorline = true                     -- Highlight cursor line
@@ -122,7 +130,7 @@ vim.opt.relativenumber = true                 -- Show relative line numbers
 vim.opt.report = 0                            -- Always report changed lines
 vim.opt.scrolloff = 4                         -- Lines of context
 vim.opt.shiftround = true                     -- Round indent
-vim.opt.shiftwidth = 0                        -- Size of an indent
+vim.opt.shiftwidth = 0                        -- Indent size
 vim.opt.shortmess = 'atToOcCF'                -- Prompt message options
 vim.opt.sidescrolloff = 12                    -- Columns of context
 vim.opt.signcolumn = 'yes'                    -- Show sign column
@@ -131,7 +139,7 @@ vim.opt.smartindent = true                    -- Insert indents automatically
 vim.opt.splitbelow = true                     -- Put new window below current
 vim.opt.splitright = true                     -- Put new window right of current
 vim.opt.tabstop = 2                           -- Number of spaces tabs count for
-vim.opt.textwidth = 99                        -- Maximum width of text
+vim.opt.textwidth = 99                        -- Max width of text
 vim.opt.updatetime = 200                      -- Delay before swap file is saved
 vim.opt.wildmode = {'list:longest'}           -- Command completion options
 vim.opt.wrap = false                          -- Disable line wrap
